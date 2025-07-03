b85165af2bfa3e35c89130f508e22766470dacfc
mimoreirac <172557112+mimoreirac@users.noreply.github.com>
2025-07-03 16:45:15 -0500

    Añade modelo base

--- carroscompartidos/models.py
+++ carroscompartidos/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
+
 
 # Create your models here.
+class Anuncio(models.Model):
+    conductor = models.CharField(max_length=100)
+    SECTORES = {
+        "NORTE": "Norte",
+        "SUR": "Sur",
+        "CENTRO": "Centro",
+        "VALLE CHILLOS": "Valle de los Chillos",
+        "CUMBAYA": "Cumbayá",
+        "TUMBACO": "Tumbaco",
+    }
+    sector = models.CharField(max_length=50, choices=SECTORES)
+    hora_salida = models.TimeField()
+    hora_llegada = models.TimeField()
+    desde_donde = models.CharField(max_length=100)
+    hacia_donde = models.CharField(max_length=100)
+    numero_contacto = PhoneNumberField(blank=False)
--- devchallenge/settings.py
+++ devchallenge/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     "widget_tweaks",
     "slippers",
     "carroscompartidos",
+    "phonenumber_field",
 ]
 
 MIDDLEWARE = [
